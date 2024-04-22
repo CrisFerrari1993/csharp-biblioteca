a252610873b6a548d27f361292c99c5f811d9bd1
Cristopher Ferrari <ferraricristopher93@gmail.com>
2024-04-22 16:26:31 +0200

    Classe Prestito completata

---
+++ csharp-biblioteca/Prestito.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_biblioteca
+{
+    public class Prestito
+    {
+        public Prestito(string dataInizio, string dataFine, Documento documentoPrestato, Utenti utentePrestito)
+        {
+            DataInizio = dataInizio;
+            DataFine = dataFine;
+            this.documentoPrestato = documentoPrestato;
+            UtentePrestito = utentePrestito;
+        }
+
+        public string DataInizio { get; set; }
+        public string DataFine { get; set; }
+        public Documento documentoPrestato { get; set; }
+        public Utenti UtentePrestito { get; set; }
+    }
+}
