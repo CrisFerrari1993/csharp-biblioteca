1b3a65e1b8071f928c00be5c7233f900f9ffb6aa
Cristopher Ferrari <ferraricristopher93@gmail.com>
2024-04-22 16:14:48 +0200

    Utenti rifatti per errore di logica

---
+++ csharp-biblioteca/Utenti.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_biblioteca
+{
+    public class Utenti
+    {
+        public Utenti(string cognome, string nome, string email, string password, string recapitoTelefonico)
+        {
+            Cognome = cognome;
+            Nome = nome;
+            Email = email;
+            Password = password;
+            RecapitoTelefonico = recapitoTelefonico;
+        }
+
+        public string Cognome { set; get; }
+        public string Nome { set; get; }
+        public string Email { set; get; }
+        public string Password { set; get; }
+        public string RecapitoTelefonico { set; get; }
+    }
+}
