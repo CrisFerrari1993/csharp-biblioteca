3d674793f74285f7bfedd59dd89b6ee31ba95f0b
Cristopher Ferrari <ferraricristopher93@gmail.com>
2024-04-22 16:54:51 +0200

    fix errorre

--- csharp-biblioteca/Libri.cs
+++ csharp-biblioteca/Libri.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace csharp_biblioteca
 {
-    public class Libri
+    public class Libri : Documento
     {
         public Libri(string titolo, int anno, string settore, bool stato, double scaffale, string autore, string ISBN, int numeroDiPagine) : base(titolo, anno, settore, stato, scaffale, autore, ISBN)
         {
