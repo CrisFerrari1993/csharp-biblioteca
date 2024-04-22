46a9ce2da8e692128082dfe6d37e869a3347a391
Cristopher Ferrari <ferraricristopher93@gmail.com>
2024-04-22 16:52:21 +0200

    Funzione che genera dei documenti completata

--- csharp-biblioteca/Biblioteca.cs
+++ csharp-biblioteca/Biblioteca.cs
@@ -31,5 +31,25 @@ namespace csharp_biblioteca
             Utenti utenti = new Utenti(cognome, nome, email, password, recapitoTelefonico);
             utentiLista.Add(utenti);
         }
+
+        //creazione funzione per generare libri e dvd
+        public void GeneraDocumenti()
+        {
+            //Libri
+            Libri ichiQHachiYon = new Libri("1Q84", 2009, "Thriller", true, 10.12f, "Harui Murakami", "13D2324AD4", 653);
+            documentiLista.Add(ichiQHachiYon);
+            Libri norWood = new Libri("Norvegian Wood", 1987, "Romantico", true, 13.22f, "Harui Murakami", "13D2324AD1", 410);
+            documentiLista.Add(norWood);
+            Libri uccelloCheGiraLeViti = new Libri("L'uccello che girava le viti del mondo", 1994, "Romanzo", true, 8.12f, "Harui Murakami", "13D2324AD4", 522);
+            documentiLista.Add(uccelloCheGiraLeViti);
+
+            //DvD
+            DvD cenerentola = new DvD("cenerentola", 1996, "animazione", true, 15.14f, "Lorenzo Rossi", "235F78G2F3", 70);
+            documentiLista.Add(cenerentola);
+            DvD biancaneve = new DvD("biancaneve", 1996, "animazione", true, 15.14f, "Lorenzo Rossi", "234DA79NK7", 70);
+            documentiLista.Add(biancaneve);
+            DvD godzilla = new DvD("godzilla", 1996, "animazione", false, 15.14f, "Lorenzo Rossi", "87A6FG76T9", 70);
+            documentiLista.Add(godzilla);
+        }
     }
 }
