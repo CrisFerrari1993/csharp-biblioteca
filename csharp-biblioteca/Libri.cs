61481c66bdfe9c0b30b075375479c723b2556cd9
Cristopher Ferrari <ferraricristopher93@gmail.com>
2024-04-22 16:23:55 +0200

    Classe libri done

---
+++ csharp-biblioteca/Libri.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_biblioteca
+{
+    public class Libri
+    {
+        public Libri(string titolo, int anno, string settore, bool stato, double scaffale, string autore, string ISBN, int numeroDiPagine) : base(titolo, anno, settore, stato, scaffale, autore, ISBN)
+        {
+            NumeroDiPagine = numeroDiPagine;
+        }
+
+        public int NumeroDiPagine { set; get; }
+    }
+}
