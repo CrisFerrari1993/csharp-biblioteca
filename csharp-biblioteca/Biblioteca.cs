d16736b93c0ec411fcf0e3caa34464860ab4f5bf
Cristopher Ferrari <ferraricristopher93@gmail.com>
2024-04-22 16:39:11 +0200

    funzione per registrare utente fatta

--- csharp-biblioteca/Biblioteca.cs
+++ csharp-biblioteca/Biblioteca.cs
@@ -13,6 +13,23 @@ namespace csharp_biblioteca
         public List<Documento> documentiLista = new List<Documento>();
         public List<Prestito> prestitiLista = new List<Prestito>();
 
+        // funzione per registrare utente
+        public void RegistraUtente()
+        {
+            Console.WriteLine("Registrati");
+            Console.Write("Inserisci il tuo congnome: ");
+            string cognome = Console.ReadLine();
+            Console.Write("Inserisci il tuo nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Inserisci la tua email: ");
+            string email = Console.ReadLine();
+            Console.Write("Inserisci la tua password: ");
+            string password = Console.ReadLine();
+            Console.Write("Inserisci il tuo recapito telefonico: ");
+            string recapitoTelefonico = Console.ReadLine();
 
+            Utenti utenti = new Utenti(cognome, nome, email, password, recapitoTelefonico);
+            utentiLista.Add(utenti);
+        }
     }
 }
