92391a57ceee0a38b05322a87a13a5062663c838
Cristopher Ferrari <ferraricristopher93@gmail.com>
2024-04-22 15:17:09 +0200

    classe utente finita

--- csharp-biblioteca/Utente.cs
+++ csharp-biblioteca/Utente.cs
@@ -18,7 +18,16 @@ namespace csharp_biblioteca
         public string Nome { get { return _nome; } set { _nome = value; } }
         public string Email { get { return _email; } set { _email = value; } }
         public string Password { set { _password = value; } }
-        public string RecepcaoTelefonico { get {  return _recapitoTelefonico;} set { _recapitoTelefonico = value; } }
-
+        public string RecapitoTelefonico { get {  return _recapitoTelefonico;} set { _recapitoTelefonico = value; } }
+        
+        //Costruuttore
+        public Utente(string nome, string cognome, string email, string password, string telefono)
+        {
+            this._nome = nome;
+            this._cognome = cognome;
+            this._email = email;
+            this._password = password;
+            this._recapitoTelefonico = telefono;
+        }
     }
 }
